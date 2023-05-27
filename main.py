863f7d01f7593439e60256c903de1ef8c593cff1
Jeison Moncayo <desarrollo5ude@udea.edu.co>
2023-05-27 13:15:32 -0500

    Configurado cors

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+from fastapi.middleware.cors import CORSMiddleware
 
 model = joblib.load("modelo/modelo_rfr.pkl")
 scaler_input = joblib.load("modelo/minMax_scaler_input.pkl")
@@ -11,11 +12,21 @@ scaler_output = joblib.load("modelo/minMax_scaler_output.pkl")
 
 app = FastAPI(debug=True)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 #modelo = joblib.load('modelo/modelo.pkl')
 
 
 class Entrada(BaseModel):
-    datos: List[List[float]]
+    datos: List[
+        List[float]
+    ]
 
 
 @app.get("/")
@@ -26,6 +37,7 @@ def index():
 @app.post("/predict")
 def predict(input_data: Entrada):
     # Convertir los datos de entrada a un DataFrame
+    print('Datos de entrada: ' + str(input_data.datos))
     input_df = pd.DataFrame(input_data.datos)
     print('Datos convertidos a dataframe, shape: ' + str(input_df.shape))
 
